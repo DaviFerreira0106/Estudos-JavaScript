d7ced156585251b28929e78583bf1cb8cbfcb29a
Davi Ferreira <123248131+DaviFerreira0106@users.noreply.github.com>
2024-02-20 12:17:55 -0300

    Trabalhando com estruturas condicionais

---
+++ Modulo D/exercicio001/condicoes.js
@@ -0,0 +1,8 @@
+// Trabalhando com estruturas condicionais no JavaScript
+
+var vel = 70.0
+console.log(`Sua velocidade no veiculo é ${vel}Km/h`);
+if(vel > 60) { // Condição Simples
+    console.log(`Sua velocidade está acima do permitido na via. MULTADO!`);
+}
+console.log(`Use sempre cinto de segurança em seu veiculo!`);
---
+++ Modulo D/exercicio002/condicoes.js
@@ -0,0 +1,6 @@
+var pais = 'BRA';
+if(pais == 'Brasil' || pais == 'BRA') {
+    console.log(`Você é Brasileiro`);
+} else {
+    console.log(`Você é Estrangeiro`);
+}
