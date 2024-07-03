a7fb743ab58f4887149153fefd8ac7c7caf4d826
Davi Ferreira <123248131+DaviFerreira0106@users.noreply.github.com>
2024-07-03 19:17:41 -0300

    Exericios de condição em javaScript

---
+++ Modulo D/exercicio008/condicaoComposta001.js
@@ -0,0 +1,7 @@
+var pais = 'EUA';
+console.log(`Você está no país ${pais}`);
+if(pais.toLowerCase() == 'brasil'){
+    console.log(`Você é Brasileiro`);
+}else {
+    console.log(`Você é estrangeiro`);
+}
---
+++ Modulo D/exercicio008/condicaoSimples001.js
@@ -0,0 +1,6 @@
+var vel = 70;
+console.log(`A sua velócidade é de ${vel} Km/h`);
+if(vel > 60){
+    console.log(`Você ultrapassou a velócidade permitida. MULTADO!`);
+}
+console.log(`Use sempre cinto de segurança!`);
